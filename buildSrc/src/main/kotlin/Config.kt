5a7f05088e4e0f4dcbae0470fd06505e295fe4c0
Sidali Mellouk <s.mellouk@outlook.com>
2020-03-15 13:04:19 +0100

    Task-1 [Prj][New]: Create project configuration
    Created project configurations:
    * Included buildSrc module to handle dependecies and versions
    * Included repositories module which will represent data repos for our domain
    * Included features which will hold different app features
    * Added feature user-list which will represent screen 1
    * Added feature user-profile wich will represent screen 2

---
+++ buildSrc/src/main/kotlin/Config.kt
@@ -0,0 +1,11 @@
+import org.gradle.api.JavaVersion
+
+object Config {
+    const val compileSdkVersion = 29
+    const val buildToolsVersion = "29.0.3"
+    const val appId = "io.mellouk.usershub"
+    const val mindSdkVersion = 16
+    const val targetSdkVersion = 29
+    const val javaVersion = "1.8"
+
+}
